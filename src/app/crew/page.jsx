2cb5b1cb325cdca84ef9a3b1abaeb3fffe72a957
ahlmy <ahmethilmiyoruk@gmail.com>
2023-06-08 15:24:04 +0300

    Last pages: tech, crew

--- src/app/crew/page.jsx
+++ src/app/crew/page.jsx
@@ -1,11 +1,111 @@
+"use client";
+import { useState } from "react";
+import Image from "next/image";
 import styles from "./page.module.css";
+import {
+  douglas_hurley,
+  anousheh_ansari,
+  mark_shuttleworth,
+  victor_glover,
+} from "@/assets/crew";
 
-
+const crewMembers = {
+  douglas: {
+    name: "douglas hurley",
+    title: "commander",
+    desc: `Douglas Gerald Hurley is an American engineer, former Marine Corps pilot 
+  and former NASA astronaut. He launched into space for the third time as 
+  commander of Crew Dragon Demo-2.`,
+    img: douglas_hurley,
+  },
+  anousheh: {
+    name: "Anousheh Ansari",
+    title: "Flight Engineer",
+    desc: `Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. 
+  Ansari was the fourth self-funded space tourist, the first self-funded woman to 
+  fly to the ISS, and the first Iranian in space. `,
+    img: anousheh_ansari,
+  },
+  victor: {
+    name: "Victor Glover",
+    title: "Pilot",
+    desc: `Pilot on the first operational flight of the SpaceX Crew Dragon to the 
+  International Space Station. Glover is a commander in the U.S. Navy where 
+  he pilots an F/A-18.He was a crew member of Expedition 64, and served as a 
+  station systems flight engineer. `,
+    img: victor_glover,
+  },
+  mark: {
+    name: "Mark Shuttleworth",
+    title: "Mission Specialist",
+    desc: `Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind 
+  the Linux-based Ubuntu operating system. Shuttleworth became the first South 
+  African to travel to space as a space tourist.`,
+    img: mark_shuttleworth,
+  },
+};
 
 const Crew = () => {
+  const [member, setMember] = useState("douglas");
   return (
     <main>
-      <div className={styles.crew}></div>
+      <div className={styles.crew}>
+        <div className={styles.leftContainer}>
+          <div className={styles.left}>
+            <h2>meet your crew</h2>
+            <div className={styles.member}>
+              <h2 className={styles.title}>{crewMembers[member].title}</h2>
+              <h1 className={styles.name}>{crewMembers[member].name}</h1>
+              <p className={styles.desc}>{crewMembers[member].desc}</p>
+            </div>
+            <div className={styles.nav}>
+              <span
+                onClick={() => setMember("douglas")}
+                style={
+                  member === "douglas"
+                    ? { backgroundColor: "white", scale: 1.2 }
+                    : null
+                }
+              ></span>
+              <span
+                onClick={() => setMember("anousheh")}
+                style={
+                  member === "anousheh"
+                    ? { backgroundColor: "white", scale: 1.2 }
+                    : null
+                }
+              ></span>
+              <span
+                onClick={() => setMember("victor")}
+                style={
+                  member === "victor"
+                    ? { backgroundColor: "white", scale: 1.2 }
+                    : null
+                }
+              ></span>
+              <span
+                onClick={() => setMember("mark")}
+                style={
+                  member === "mark"
+                    ? { backgroundColor: "white", scale: 1.2 }
+                    : null
+                }
+              ></span>
+            </div>
+          </div>
+        </div>
+        <div className={styles.rightContainer}>
+          <div className={styles.right}>
+            <Image
+              src={crewMembers[member].img}
+              alt={crewMembers[member].name}
+              style={
+                member === "mark" ? { transform: "rotateY(180deg)" } : null
+              }
+            />
+          </div>
+        </div>
+      </div>
     </main>
   );
 };
--- src/app/technology/page.jsx
+++ src/app/technology/page.jsx
@@ -1,9 +1,116 @@
+"use client";
+import Image from "next/image";
 import styles from "./technology.module.css";
+import { useState, useEffect } from "react";
+import {
+  launch_vehicle_landscape,
+  launch_vehicle_portrait,
+  space_capsule_landscape,
+  space_capsule_portrait,
+  spaceport_landscape,
+  spaceport_portrait,
+} from "@/assets/technology";
+
+const techInfo = {
+  capsule: {
+    name: "Space capsule",
+    desc: `A space capsule is an often-crewed spacecraft that uses a blunt-body reentry 
+  capsule to reenter the Earth's atmosphere without wings. Our capsule is where 
+  you'll spend your time during the flight. It includes a space gym, cinema, 
+  and plenty of other activities to keep you entertained.`,
+    img_landscape: space_capsule_landscape,
+    img_portrait: space_capsule_portrait,
+  },
+  spaceport: {
+    name: "Spaceport",
+    desc: `A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, 
+  by analogy to the seaport for ships or airport for aircraft. Based in the 
+  famous Cape Canaveral, our spaceport is ideally situated to take advantage 
+  of the Earth’s rotation for launch.`,
+    img_landscape: spaceport_landscape,
+    img_portrait: spaceport_portrait,
+  },
+  vehicle: {
+    name: "Launch vehicle",
+    desc: `A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a 
+  payload from Earth's surface to space, usually to Earth orbit or beyond. Our 
+  WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, 
+  it's quite an awe-inspiring sight on the launch pad!`,
+    img_landscape: launch_vehicle_landscape,
+    img_portrait: launch_vehicle_portrait,
+  },
+};
+
+const media = window.matchMedia("(max-width: 800px");
 
 const Technology = () => {
+  const [tech, setTech] = useState("vehicle");
+  const [mobile, setMobile] = useState();
+  useEffect(() => {
+    media.addEventListener("change", (e) => {
+      e.matches ? setMobile(true) : setMobile(false);
+    });
+  }, []);
   return (
     <main>
-      <div className={styles.technology}></div>
+      <div className={styles.technology}>
+        <div className={styles.leftContainer}>
+          <div className={styles.left}>
+            <h2>space launch 101</h2>
+            <div className={styles.tech}>
+              <div className={styles.nav}>
+                <span
+                  onClick={() => setTech("vehicle")}
+                  style={
+                    tech === "vehicle"
+                      ? { backgroundColor: "white", color: "black" }
+                      : null
+                  }
+                >
+                  1
+                </span>
+                <span
+                  onClick={() => setTech("capsule")}
+                  style={
+                    tech === "capsule"
+                      ? { backgroundColor: "white", color: "black" }
+                      : null
+                  }
+                >
+                  2
+                </span>
+                <span
+                  onClick={() => setTech("spaceport")}
+                  style={
+                    tech === "spaceport"
+                      ? { backgroundColor: "white", color: "black" }
+                      : null
+                  }
+                >
+                  3
+                </span>
+              </div>
+              <div className={styles.info}>
+                <h2>THE TERMINOLOGY...</h2>
+                <h1>{techInfo[tech].name}</h1>
+                <p>{techInfo[tech].desc}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.rightContainer}>
+          <div className={styles.right}>
+            <Image
+              src={
+                mobile
+                  ? techInfo[tech].img_landscape
+                  : techInfo[tech].img_portrait
+              }
+              alt={techInfo[tech].name}
+            />
+          </div>
+        </div>
+      </div>
     </main>
   );
 };
